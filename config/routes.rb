40079278eeb252194cc819212c2e565d6efefa91
aya-yay <69228983+aya-yay@users.noreply.github.com>
2020-08-11 16:08:46 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # root "messages#index" #ルートパスへのアクセスがあったら、messages_controllerのindexアクションが呼び出されるように。
   root "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
     # ↑メッセージを投稿する際には、どのルームで投稿されたメッセージなのかをパスから判断できるようにしたいので、ルーティングのネストを利用。「チャットルームに属しているメッセージ」と言う意味。これによって、メッセージに結びつくルームのidの情報を含んだパスを、受け取れるようになります。
   end
