1f86b11c968a2d2bf2a57b7903931e67ee5b8266
aya-yay <69228983+aya-yay@users.noreply.github.com>
2020-08-11 11:35:17 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,22 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create
+    #↑binding.pryで、pryにparamsと打ち込むと{"name"=>"新規チャット", "user_ids"=>["", "1", "2"]}が出てくるので、「user_ids」キーに「所属ユーザーのidの配列」が送信されていることがわかります。つまり、ビュー側からコントローラー側へ正しく値が送られていることが確認できました。
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids:[])
+  end
+
 end
+
