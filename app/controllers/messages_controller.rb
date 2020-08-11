1427663444eae609dad4eed180c605a5035c1a03
aya-yay <69228983+aya-yay@users.noreply.github.com>
2020-08-11 14:44:37 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,35 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+      # @roomには、Room.find(params[:room_id])と記述することで、paramsに含まれているroom_idを代入。
+      # 紐解いて説明すると、直前の問題にあった通りルーティングをネストしているため/rooms/:room_id/ messagesといったパスになる。# パスにroom_idが含まれているため、paramsというハッシュオブジェクトの中に、room_idという値が存在。そのため、params[:room_id]と記述することでroom_idを取得できる。
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    @message.save
+    # @room.messages.newでチャットルームに紐づいたメッセージのインスタンスを生成し、message_paramsを引数にして、privateメソッドを呼び出します。その値を@messageに代入し、saveメソッドでメッセージの内容をmessagesテーブルに保存。
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+    # createアクション内に、メッセージを保存できた場合とできなかった場合で条件分岐の処理を行う。@message.saveでメッセージの保存に成功した場合、room_messages_path(@room)
+    # (参加しているチャットルームに投稿したメッセージの一覧画面）にリダイレクトし、失敗した場合、indexアクションが実行され、同じページに戻るという記述。
+  end
+
+
+  
+
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    # パラメーターの中に、ログインしているユーザーのidと紐付いている、メッセージの内容(content)を受け取れるように許可
+  end
+
 end
+
