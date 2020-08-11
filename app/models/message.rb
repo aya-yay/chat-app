8ed1182233bfb5e2030d4bf650dba95e980fa573
aya-yay <69228983+aya-yay@users.noreply.github.com>
2020-08-11 16:53:57 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,10 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached?
+                                      # テキストが存在していなくても、画像の投稿が可能となった
+
+  def was_attached?
+    self.image.attached?
+  end
 end
