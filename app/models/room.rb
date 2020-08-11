e4e01c0537390c86fd57064666e0c15c5eb19705
aya-yay <69228983+aya-yay@users.noreply.github.com>
2020-08-11 12:05:22 +0900

    チャット画面の編集

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,7 @@ class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
 
-  validates :name, presemce: true
+  validates :name, presence: true
   # 　　　　　　↑ルーム名のこと
 end
 
